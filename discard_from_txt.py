e66a8f3aa703282d34a7c449e96b1e0a03e64a95
Fabián Villena <villenafabian@gmail.com>
2020-04-15 23:06:48 -0400

    discard from txt

---
+++ discard_from_txt.py
@@ -0,0 +1,13 @@
+import argparse
+from src import Discard
+
+parser = argparse.ArgumentParser(description='Discard diagnostic from txt file list.', usage='%(prog)s [options]')
+parser.add_argument("--from_folder", help="Directory where the diagnostics are.", default="samples/")
+parser.add_argument("--to_folder", help="Directory where the rejected diagnostics are.", default="samples_rejected/")
+parser.add_argument("--file_list", help="Text file where the arguments to reject are, one filename per line.", required=True)
+
+args = parser.parse_args()
+
+d = Discard(args.from_folder,args.to_folder)
+d.from_txt(args.file_list)
+d.discard()
