ade4d0a9537e3e140280e56c7b6efb87a5bc94fb
Fabián Villena <villenafabian@gmail.com>
2020-04-15 23:11:40 -0400

    sample picker command line

---
+++ pick_sample.py
@@ -0,0 +1,13 @@
+import argparse
+from src import SamplePicker
+
+parser = argparse.ArgumentParser(description='Diagnostic sample picker', usage='%(prog)s [options]')
+parser.add_argument("--corpus", help="Location of the JSON corpus.", default="corpus.json")
+parser.add_argument("--samples_folder", help="Directory where the diagnostics are.", default="samples/")
+parser.add_argument("--samples_rejected_folder", help="Directory where the rejected diagnostics are.", default="samples_rejected/")
+parser.add_argument("-n", help="number of diagnoses to pick.", required=True, type=int)
+
+args = parser.parse_args()
+
+p = SamplePicker(args.corpus,args.samples_folder,args.samples_rejected_folder)
+p.pick(args.n)
